b6b4d129bef856f77cdc2b9bab0def9ceec51fd8
Thiago <thiagonascimentodeveloper@gmail.com>
2022-07-05 15:10:36 -0300

    atualizado o sistema de transição ao navegar e adicionados  novos projetos para a seção projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,27 +9,36 @@ function selecionarItem(item) {
     if(item == 1) {
         div_selected.style.left = '1rem';
         projects.style.opacity = '0';
-        skills.style.opacity = '1';
-        skills.style.width = '100%';
-        projects.style.width = '0';
+        setTimeout(() => {
+            projects.style.display = 'none';
+            skills.style.display = 'grid';
+            skills.style.opacity = '1';
+        },500);
 
     } else {
         div_selected.style.left = '12rem';
         skills.style.opacity = '0';
-        skills.style.width = '0';
-        projects.style.opacity = '1';
-        projects.style.width = '100%';
+        projects.style.opacity = '0';
+        setTimeout(() => {
+            skills.style.display = 'none';
+            projects.style.display = 'grid';
+            projects.style.opacity = '1';
+        },500);
     }
 };
 
 function abrirProjeto(n) {
     if (n == 1) {
-        window.location.href = 'https://github.com/Thiago-Nasc/game-mata-mosquito';
+        window.location.href = 'https://github.com/Thiago-Nasc/Dictionary';
     } else if (n == 2) {
-        window.location.href = 'https://github.com/Thiago-Nasc/app-orcamento-pessoal';
+        window.location.href = 'https://github.com/Thiago-Nasc/Personal-Budget';
     } else if (n == 3) {
-        window.location.href = 'https://github.com/Thiago-Nasc/calculator';
+        window.location.href = 'https://github.com/Thiago-Nasc/Calculator';
     } else if (n == 4) {
-        window.location.href = 'https://github.com/Thiago-Nasc/Music-Library-Page';
+        window.location.href = 'https://github.com/Thiago-Nasc/Music-Library';
+    } else if (n == 5) {
+        window.location.href = 'https://github.com/Thiago-Nasc/Kill-Mosquito';
+    } else if (n == 6) {
+        window.location.href = 'https://github.com/Thiago-Nasc/Portfolio';
     }
-};
+}
